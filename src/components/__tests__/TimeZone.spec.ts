b231ae1193fcebc522ecc15ac024004e071073da
Rodrigues <rsrodrigues2@gmail.com>
2023-05-18 16:49:44 -0300

    test(ISSUE): unit tests

--- src/components/__tests__/TimeZone.spec.ts
+++ src/components/__tests__/TimeZone.spec.ts
@@ -1,11 +1,20 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import TimeZone from './TimeZone.vue'
+import TimeZone from './../TimeZone.vue'
 
 describe('TimeZone', () => {
+  const wrapper = mount(TimeZone, { props: { msg: 'TimeZone around the World!' } })
   it('renders properly', () => {
-    const wrapper = mount(TimeZone, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    expect(wrapper.text()).toContain('TimeZone around the World!')
+  })
+  it('Check if every element in the array is an object', () => {
+    const vm = wrapper.vm;
+    vm.getAllTimes();
+    expect(vm.allTimes).to.be.an('array').that.is.not.empty
+    const isArrayOfObjects = vm.allTimes.every(item => typeof item === 'object' && item !== null);
+
+    // Assert the expected result
+    expect(isArrayOfObjects).to.be.true;
   })
 })
